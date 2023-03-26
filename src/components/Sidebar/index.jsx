1f19b4d17289b03a7ced2560f009cbb13fe3f46b
ilhomjon003 <ilhomjonisoqjonov2@gmail.com>
2023-03-26 20:52:14 +0500

    sidebar is added

--- src/components/Sidebar/index.jsx
+++ src/components/Sidebar/index.jsx
@@ -1,10 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 import { TbArrowBigRightLinesFilled } from "react-icons/tb";
 import { Nav_Routes } from "../../routes";
 import c from "./style.module.css";
 import { Link } from "react-router-dom";
 
 const Sidebar = () => {
+  const [isMoreMoved, setIsMoreMoved] = useState(false);
   return (
     <div className={c.sidebar}>
       <div className={c.sidebar__links}>
@@ -14,11 +15,25 @@ const Sidebar = () => {
           </div>
         ))}
         <div className={`${c.sidebar__link} flex`}>
-          <dwiv className={`${c.dropdown__wrapper} flex`}>
+          <div
+            className={`${c.dropdown__wrapper} flex`}
+            onClick={() => setIsMoreMoved(true)}
+          >
             <p>More</p>
             <TbArrowBigRightLinesFilled className={c.more__icon} />
-          </dwiv>
-          <div className={c.more__link}>
+          </div>
+          <div
+            className={
+              isMoreMoved ? `${c.more__link} ${c.moved}` : `${c.more__link}`
+            }
+          >
+            <div
+              className={`${c.back__wrapper} flex`}
+              onClick={() => setIsMoreMoved(false)}
+            >
+              <TbArrowBigRightLinesFilled className={c.back__icon} />
+              <p>Back</p>
+            </div>
             {["battle pass", "crew", "competetive", "v-bucks", "help"].map(
               (li__item) => (
                 <li key={li__item}>{li__item}</li>
