1030a203447695bf6c7aac37c7fbca408e3dc398
ilhomjon003 <ilhomjonisoqjonov2@gmail.com>
2023-03-29 06:12:12 +0500

    vbucks fixed

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,20 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import Sidebar from "./components/Sidebar";
 import Home from "./pages/Home";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, useLocation } from "react-router-dom";
 import VBucks from "./pages/Vbucks";
 import BattlePass from "./pages/BattlePass";
 import Crew from "./pages/Crew";
 
 function App() {
   const [isSidebarOpened, setIsSidebarOpened] = useState(false);
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    setIsSidebarOpened(false);
+  }, [pathname]);
 
   isSidebarOpened
     ? (document.body.style = "overflow: hidden;")
--- src/pages/Crew/index.jsx
+++ src/pages/Crew/index.jsx
@@ -4,14 +4,14 @@ import "./style.css"
 const Crew = () => {
   return (
     <div className="bucks">
-      <div className="bucks_container">
-        <div className="bucks_img">
+      <div className="bucks__container">
+        <div className="bucks__img">
           <img
             src="https://cdn2.unrealengine.com/faq-singular-card-102721-739x1023-651e0791cffe.png"
             alt=""
           />
         </div>
-        <div className="container_redeem">
+        <div className="container__redeem">
           <h1>redeem your v-bucks card</h1>
           <h3>CARDS PURCHASED AT RETAIL STORES ARE REDEEMED HERE</h3>
           <p>
@@ -20,7 +20,7 @@ const Crew = () => {
             Click Get Started below to find your Epic Games account and redeem
             your V-Bucks!
           </p>
-          <button className="redeem_btn">get started</button>
+          <button className="redeem__btn">get started</button>
         </div>
       </div>
     </div>
